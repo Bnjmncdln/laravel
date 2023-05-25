9a5ad733591b7c00cd06d16d8a0c1affdeb47950
Bnjmncdln <deleonbenjamin8@gmail.com>
2023-05-25 11:15:40 +0800

    1

---
+++ resources/views/news.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>News Page</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <a href="">Landing Page</a>
+            <a href="">Home</a>
+            <a href="">Profile</a>
+            <a href="">Events</a>
+            <a href="">News</a>
+            <a href="">Contact Us</a>
+        </nav>
+    </header>
+    <h1>Welcome to News Page</h1>
+</body>
+</html>
